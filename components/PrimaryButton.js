e04f34fa9d4d4ddcac5fdd885067519190ce477d
marisobreiro <marianasobreiro93@gmail.com>
2022-06-07 14:03:03 -0300

    Customização do input container

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+function PrimaryButton({btntext}) {
+    return (
+            <View>
+                <Text>{btntext}</Text>
+            </View>
+    )
+}
+
+export default PrimaryButton;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,43 @@
-function StartGameScreen () {
+import { StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
     return (
-        <></>
+        <View style={styles.inputContainer}>
+            <TextInput 
+                style={styles.numberInput}
+                maxLength={2}
+            />
+            <PrimaryButton btntext="Recomeçar" />
+            <PrimaryButton btntext="Confirmar" />
+        </View>
     )
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        backgroundColor: '#72063c',
+        borderRadius: 8,
+        elevation: 8,
+        marginHorizontal: 24,
+        marginTop: 100,
+        padding: 16,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+    numberInput: {
+        borderBottomColor: '#DDB52F',
+        borderBottomWidth: 2,
+        color: '#DDB52F',
+        fontSize: 32,
+        fontWeight: 'bold', 
+        height: 50,
+        marginVertical: 8,
+        textAlign: 'center',
+        width: 50
+    }
+})
